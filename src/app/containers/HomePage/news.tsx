736ed104930d035e03939b792a56e3b7cad24db2
denilson87 <denilsonmucavel@outlook.com>
2021-12-10 14:35:49 +0200

    updating the news section

--- src/app/containers/HomePage/news.tsx
+++ src/app/containers/HomePage/news.tsx
@@ -6,20 +6,21 @@ import styled from "styled-components";
 const Container = styled.div`
 box-shadow: 0 1.3px 12px -3px rgba( 0, 0, 0, 0.4);
 background-color: rgb(254 226 226);
-${`
+min-height: 23em;
+max-height: 23em;
+width:100%;
 
+${`
+     
     flex
+    w-full
     flex-wrap
-    items-center
-    justify-center
     pt-4
     pb-4
     pr-7
     pl-7
     md:pl-0
     md:pr-0
-    bg-white
-
 `};
 
 `;
