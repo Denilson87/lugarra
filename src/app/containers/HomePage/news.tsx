2acba339114bdd6e85561fd615dfa0009f33d0a2
denilson87 <denilsonmucavel@outlook.com>
2021-12-10 14:50:52 +0200

    updating the the homePage

--- src/app/containers/HomePage/news.tsx
+++ src/app/containers/HomePage/news.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import styled from "styled-components";
 
-
 const Container = styled.div`
 box-shadow: 0 1.3px 12px -3px rgba( 0, 0, 0, 0.4);
 background-color: rgb(254 226 226);
